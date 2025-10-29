fe1bb2a91c5798ae02278a6dfdc79c4a383a5f94
Nehabj <neha@coact.co.in>
2025-10-29 12:47:21 +0530

    complete Project files

---
+++ src/components/index.js
@@ -0,0 +1,10 @@
+export { Card } from "./Card";
+
+export { Footer } from "./Footer";
+export { Header } from "./Header";  
+
+export { Button } from "./Button";
+
+export { ScrollToTop } from "./ScrollToTop";
+
+
---
+++ src/pages/index.js
@@ -0,0 +1,4 @@
+export { Search } from './Search'
+export { PageNotFound } from './PageNotFound'
+export { MovieList } from './MovieList'
+export { MovieDetail } from './MovieDetail'
